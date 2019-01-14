66dc5e6c40c742c0113aba564bd894f698cc4e9e
rogersgt <gradytrogers@gmail.com>
2019-01-13 21:22:12 -0500

    no loop wait in nginx

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -17,16 +17,5 @@ done
 cp /home/nginx.conf /etc/nginx/
 
 sed -i "s/TARGET_HOSTS/$NGINX_CONFIG/g; s/PORT/$PORT/g" /etc/nginx/nginx.conf
-cat /etc/nginx/nginx.conf
-
-function waitForUpstream() {
-  # use first host in the list
-  server=HOST_ARRAY[0]
-  retry=$1
-  if [ -z "$retry" ]; then
-    retry=0
-  fi
-  
-}
 
 nginx -g "daemon off;"
